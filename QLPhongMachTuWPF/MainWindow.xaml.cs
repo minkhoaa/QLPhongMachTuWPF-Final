b2a6461440c2438f19546400c62e75bab5716fcf
khoadd879 <163757386+khoadd879@users.noreply.github.com>
2024-12-07 18:57:52 +0700

    big update

--- QLPhongMachTuWPF/MainWindow.xaml.cs
+++ QLPhongMachTuWPF/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace QLPhongMachTuWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isSidebarCollapsed = false;
         public MainWindow()
         {
+
             InitializeComponent();
+        }
+        private void ToggleSidebar_Click(object sender, RoutedEventArgs e)
+        {
+            if (isSidebarCollapsed)
+            {
+                // Mở rộng Sidebar
+                SidebarColumn.Width = new GridLength(200);
+                text1.Visibility = Visibility.Visible;
+                text2.Visibility = Visibility.Visible;
+                logout.Visibility = Visibility.Visible;
+                management.IsEnabled = true;
+                medicine.IsEnabled = true;
+                img.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                // Thu nhỏ Sidebar
+                SidebarColumn.Width = new GridLength(60);
+                text1.Visibility = Visibility.Collapsed;
+                text2.Visibility = Visibility.Collapsed;
+                logout.Visibility = Visibility.Collapsed;
+                management.IsEnabled = false;
+                medicine.IsEnabled = false;
+                img.Visibility = Visibility.Collapsed;
+            }
+
+            isSidebarCollapsed = !isSidebarCollapsed; // Cập nhật trạng thái
         }
     }
 }
---
+++ QLPhongMachTuWPF/ViewModel/Navigation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace QLPhongMachTuWPF.ViewModel
+{
+    internal class Navigation : ViewModelBase
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand HomePageCommand { get; set; }
+        public ICommand PatientsCommand { get; set; }
+        public ICommand StaffsCommand { get; set; }
+
+
+        public ICommand ManageAppointmentCommand { get; set; }
+        public ICommand DiagnosisCommand { get; set; }
+        public ICommand ListsCommand { get; set; }
+        public ICommand InvoiceCommand { get; set; }
+
+
+        private void HomePage(object obj) => CurrentView = new HomepageVM();
+        private void Patients(object obj) => CurrentView = new PatientsVM();
+        private void Staffs(object obj) => CurrentView = new StaffsVM();
+        private void ManageAppointment(object obj) => CurrentView = new ManageAppointmentsVM();
+        private void Diagnosis(object obj) => CurrentView = new DiagnosisVM();
+        private void Lists(object obj) => CurrentView = new ListsVM();
+        private void Invoice(object obj) => CurrentView = new InvoiceVM();
+
+        public Navigation()
+        {
+            HomePageCommand = new RelayCommand(HomePage);
+            PatientsCommand = new RelayCommand(Patients);
+            StaffsCommand = new RelayCommand(Staffs);
+            ManageAppointmentCommand = new RelayCommand(ManageAppointment);
+            DiagnosisCommand = new RelayCommand(Diagnosis);
+            ListsCommand = new RelayCommand(Lists);
+            InvoiceCommand = new RelayCommand(Invoice);
+
+            CurrentView = new HomepageVM();
+        }
+    }
+}
