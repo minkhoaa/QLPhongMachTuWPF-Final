5573bca1a98fc19e4c07b02273063a4313287806
khoadd879 <163757386+khoadd879@users.noreply.github.com>
2024-12-10 00:16:03 +0700

    update datagrid

--- QLPhongMachTuWPF/View/Patients.xaml.cs
+++ QLPhongMachTuWPF/View/Patients.xaml.cs
@@ -24,21 +24,9 @@ namespace QLPhongMachTuWPF.View
         public Patients()
         {
             InitializeComponent();
-            ObservableCollection<PatientsSource> patients = new ObservableCollection<PatientsSource>();
-            patients.Add(new PatientsSource { PatientID = "9012401", PatientName = "Tran Dang Khoa", DOB = "27/10/2005", PatientAddress = "Quan 1 ahflkajlkfjalkfj", PatientPhone = "123155151", Gender = "Male", Status = "Stable" });
-            patients.Add(new PatientsSource { PatientID = "9012401", PatientName = "Tu Minh Khoa", DOB = "27/10/2005", PatientAddress = "Quan 1", PatientPhone = "123155151", Gender = "Male", Status = "Stable" });
-            patientsDatagrid.ItemsSource = patients;
+           
         }
-        public class PatientsSource
-        {
-            public string PatientID { get; set; }
-            public string PatientName { get; set; }
-            public string DOB { get; set; }
-            public string PatientAddress { get; set; }
-            public string PatientPhone { get; set; }
-            public string Gender { get; set; }
-            public string Status { get; set; }
-        }
+        
         private void AddPatientButton_Click(object sender, RoutedEventArgs e)
         {
             // Mở cửa sổ AddPatientWindow
