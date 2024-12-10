bae6dad4c750e164913653ed9339371376318952
minkhoaa <tukhoa040505@gmail.com>
2024-12-10 14:00:48 +0700

    chinh sua nut

--- QLPhongMachTuWPF/ViewModel/PatientsVM.cs
+++ QLPhongMachTuWPF/ViewModel/PatientsVM.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using QLPhongMachTuWPF.View;
 
 
@@ -12,16 +13,21 @@ namespace QLPhongMachTuWPF.ViewModel
     public class PatientsVM: ViewModelBase
     {
         public ObservableCollection<Patient> PatientsList { get; set; }
+        public ICommand CommandDeletePatients { get; set; }
 
         public PatientsVM()
         {
             // Khởi tạo danh sách bệnh nhân
             PatientsList = new ObservableCollection<Patient>
-        {
+         {
             new Patient { PatientID = "P001", Name = "John Doe", DOB = "01/01/1990", Address = "123 Main Stasfaaaaaaaaaaaaaaaaaaaaaaaa", Phone = "1234567890", Gender = "Male" },
             new Patient { PatientID = "P002", Name = "Jane Smith", DOB = "05/05/1995", Address = "456 Oak St", Phone = "9876543210", Gender = "Male" },
 
-        };
+          };
+            CommandDeletePatients = new RelayCommand<object>((p) => { return true; }, (p) => {
+                AddPatient addPatientWindow = new AddPatient();
+                addPatientWindow.ShowDialog();
+            });
             
         }
 
