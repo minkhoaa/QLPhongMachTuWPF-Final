7d579eb7f9e8a532908d1e686b543dbb71b3e957
minkhoaa <tukhoa040505@gmail.com>
2024-12-11 01:47:00 +0700

    load database medicine

--- QLPhongMachTuWPF/ViewModel/ListsVM.cs
+++ QLPhongMachTuWPF/ViewModel/ListsVM.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using QLPhongMachTuWPF.Model;
+using QLPhongMachTuWPF.View;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace QLPhongMachTuWPF.ViewModel
 {
-    internal class ListsVM
+    public class ListsVM : ViewModelBase
     {
+        public ICommand AddStaffCommand { get; set; }
+        private ObservableCollection<THUOC> _medicine;
+        public ObservableCollection<THUOC> MedicineList { get => _medicine; set { _medicine = value; OnPropertyChanged(); } }
+        public ListsVM()
+        {
+            MedicineList = new ObservableCollection<THUOC>(DataProvider.Ins.db.THUOCs);
+            AddStaffCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                AddStaff add = new AddStaff();
+                add.ShowDialog();
+
+            }
+            );
+        }
     }
 }
