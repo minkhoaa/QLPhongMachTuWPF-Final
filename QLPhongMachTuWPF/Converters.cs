563182bb17f5e5413eaf11d5f632208c6db74368
minkhoaa <tukhoa040505@gmail.com>
2024-12-13 04:55:15 +0700

    change datagrid visualization

---
+++ QLPhongMachTuWPF/Converters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace QLPhongMachTuWPF
+{
+    public  class Converters : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int status)
+            {
+                return status == 1 ? "Available" : "Unavailable";
+            }
+            return "Unknown";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Chuyển ngược lại, nếu cần
+            if (value is string status)
+            {
+                return status == "Available" ? 1 : 0;
+            }
+            return 0;
+        }
+    }
+}
