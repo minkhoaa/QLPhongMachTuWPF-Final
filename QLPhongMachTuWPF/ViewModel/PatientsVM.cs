a96f86d3e2e5d7beb1b3dd1a2ae9f552c9d00969
minkhoaa <tukhoa040505@gmail.com>
2024-12-12 23:49:16 +0700

    update search staffs and patients

--- QLPhongMachTuWPF/ViewModel/PatientsVM.cs
+++ QLPhongMachTuWPF/ViewModel/PatientsVM.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Messaging;
 using QLPhongMachTuWPF.Model;
@@ -17,7 +18,20 @@ namespace QLPhongMachTuWPF.ViewModel
     {
         private ObservableCollection<BENHNHAN> _patients;
         public ObservableCollection<BENHNHAN> PatientsList { get => _patients; set { _patients = value; OnPropertyChanged(); } }
+
+        private string _searchKeyword;
+        public string SearchKeyword
+        {
+            get => _searchKeyword;
+            set
+            {
+                _searchKeyword = value;
+                OnPropertyChanged();
+                FilterPatients(); // Gọi hàm lọc mỗi khi từ khóa thay đổi
+            }
+        }
         public ICommand AddPatientCommand { get; set; }
+        public ICollectionView FilteredPatients { get; set; }
 
         public PatientsVM()
         {
@@ -25,6 +39,7 @@ namespace QLPhongMachTuWPF.ViewModel
             PatientsList = new ObservableCollection<BENHNHAN>(DataProvider.Ins.db.BENHNHANs);
 
             // Đăng ký nhận thông báo
+            FilteredPatients = CollectionViewSource.GetDefaultView(_patients);
             Messenger.Default.Register<BENHNHAN>(this, (newPatient) =>
             {
                 PatientsList.Add(newPatient);
@@ -37,6 +52,26 @@ namespace QLPhongMachTuWPF.ViewModel
             });
 
           
+        }
+        private void FilterPatients()
+        {
+            if (FilteredPatients == null)
+                return;
+
+            // Gán bộ lọc
+            FilteredPatients.Filter = (obj) =>
+            {
+                if (obj is BENHNHAN patient)
+                {
+                    // Kiểm tra từ khóa tìm kiếm, không phân biệt chữ hoa/chữ thường
+                    return string.IsNullOrEmpty(SearchKeyword) ||
+                           patient.TenBN?.IndexOf(SearchKeyword, StringComparison.OrdinalIgnoreCase) >= 0;
+                }
+                return false;
+            };
+
+            // Làm mới view để cập nhật kết quả lọc
+            FilteredPatients.Refresh();
         }
 
     }
--- QLPhongMachTuWPF/ViewModel/StaffsVM.cs
+++ QLPhongMachTuWPF/ViewModel/StaffsVM.cs
@@ -3,10 +3,12 @@ using QLPhongMachTuWPF.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace QLPhongMachTuWPF.ViewModel
@@ -16,10 +18,26 @@ namespace QLPhongMachTuWPF.ViewModel
     { 
         public ICommand AddStaffCommand { get; set ; }
         private ObservableCollection<NHANVIEN> _staff;
+        
+        private string _SearchKeyword {  get; set; }
+
+        public string SearchKeyword { get => _SearchKeyword; 
+            set
+            {
+                _SearchKeyword = value;
+                OnPropertyChanged ();
+                FilterStaffs ();
+            }
+        }
+
+        public ICollectionView FilteredStaffs { get; set; }
         public ObservableCollection<NHANVIEN> StaffList { get => _staff; set { _staff = value; OnPropertyChanged(); } }
         public StaffsVM()
         {
             StaffList = new ObservableCollection<NHANVIEN>(DataProvider.Ins.db.NHANVIENs);
+
+            FilteredStaffs = CollectionViewSource.GetDefaultView(_staff);
+
             AddStaffCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
                 AddStaff add = new AddStaff(); 
@@ -27,6 +45,26 @@ namespace QLPhongMachTuWPF.ViewModel
 
             }
             ); 
+        }
+        void FilterStaffs()
+        {
+            if (FilteredStaffs == null)
+                return;
+
+            // Gán bộ lọc
+            FilteredStaffs.Filter = (obj) =>
+            {
+                if (obj is NHANVIEN staffs)
+                {
+                    // Kiểm tra từ khóa tìm kiếm, không phân biệt chữ hoa/chữ thường
+                    return string.IsNullOrEmpty(SearchKeyword) ||
+                           staffs.TenNV?.IndexOf(SearchKeyword, StringComparison.OrdinalIgnoreCase) >= 0;
+                }
+                return false;
+            };
+
+            // Làm mới view để cập nhật kết quả lọc
+            FilteredStaffs.Refresh();
         }
     }
 }
