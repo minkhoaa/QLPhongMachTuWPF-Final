e05853bfbda829183e53238b6ce27dbc1e06a705
minkhoaa <tukhoa040505@gmail.com>
2024-12-17 22:44:55 +0700

    modify database

--- QLPhongMachTuWPF/Model/Model1.Context.cs
+++ QLPhongMachTuWPF/Model/Model1.Context.cs
@@ -32,5 +32,6 @@ namespace QLPhongMachTuWPF.Model
         public virtual DbSet<NHANVIEN> NHANVIENs { get; set; }
         public virtual DbSet<PHIEUKHAM> PHIEUKHAMs { get; set; }
         public virtual DbSet<THUOC> THUOCs { get; set; }
+        public virtual DbSet<LICHHEN> LICHHENs { get; set; }
     }
 }
