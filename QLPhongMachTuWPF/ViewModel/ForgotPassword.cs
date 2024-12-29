b147819bd22e51c6ace68f41c1c746af6abaf255
minkhoaa <tukhoa040505@gmail.com>
2024-12-29 17:19:42 +0700

    register

---
+++ QLPhongMachTuWPF/ViewModel/ForgotPassword.cs
@@ -0,0 +1,22 @@
+﻿using Org.BouncyCastle.Tls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QLPhongMachTuWPF.ViewModel
+{
+    public class ForgotPassword : ViewModelBase
+    {
+        private string _email {  get; set; }
+        public string Email { get => _email; set { _email = value; OnPropertyChanged(); }  }
+
+        private string _username { get; set; }
+        public string Username { get => _username; set { _username = value; OnPropertyChanged(); } }
+
+        public ICommand ResetPassword {  get; set; }
+
+    }
+}
