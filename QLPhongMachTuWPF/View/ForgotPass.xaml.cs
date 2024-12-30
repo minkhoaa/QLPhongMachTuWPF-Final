1c2a4b79b4f448bb8394d8a3c2f263f67a3952d1
julynguyen247 <julyneverdie@gmail.com>
2024-12-30 13:49:30 +0700

    update

--- QLPhongMachTuWPF/View/ForgotPass.xaml.cs
+++ QLPhongMachTuWPF/View/ForgotPass.xaml.cs
@@ -91,5 +91,22 @@ namespace QLPhongMachTuWPF.View
         {
             Application.Current?.Shutdown();
         }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+                textEmail.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textEmail.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
